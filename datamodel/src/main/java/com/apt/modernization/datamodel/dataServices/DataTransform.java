cdefbc1200698d00084dc1a1a5c7799c9899e191
Christian Schowalter <christian.schowalter@msg.group>
2020-05-26 11:56:27 +0200

    - Changed DataTransform to use SLF instead of System.out - multipleInserts-Test.

--- datamodel/src/main/java/com/apt/modernization/datamodel/dataServices/DataTransform.java
+++ datamodel/src/main/java/com/apt/modernization/datamodel/dataServices/DataTransform.java
@@ -1,16 +1,19 @@
 package com.apt.modernization.datamodel.dataServices;
 
-import com.apt.modernization.datamodel.document.Quotes;
-import com.apt.modernization.datamodel.repository.QuotesRepository;
-import org.springframework.context.annotation.Bean;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import com.apt.modernization.datamodel.document.Quotes;
+import com.apt.modernization.datamodel.repository.QuotesRepository;
 
 @Component
 @EnableMongoRepositories(basePackageClasses = QuotesRepository.class)
 public class DataTransform {
+	private static final Logger LOG = LoggerFactory.getLogger(DataTransform.class);
 
     private QuotesRepository quotesRepository;
     private List<Quotes> quotesList;
@@ -24,11 +27,7 @@ public class DataTransform {
     private void printList() {
         quotesList = quotesRepository.findAll();
         for (Quotes quotes : quotesList) {
-            System.out.println(quotes.getQuote_id() + ", " +
-                    quotes.getInsured_name() + ", " +
-                    quotes.getIncept_date() + ", " +
-                    quotes.getExpiry_date() + ", " +
-                    quotes.getCurrency());
+            LOG.trace("{}, {}, {}, {}, {}",quotes.getQuote_id(),quotes.getInsured_name(),quotes.getIncept_date(),quotes.getExpiry_date() ,quotes.getCurrency());
         }
     }
 }
--- datamodel/src/test/java/com/apt/modernization/datamodel/PersistenceTest.java
+++ datamodel/src/test/java/com/apt/modernization/datamodel/PersistenceTest.java
@@ -3,6 +3,8 @@ package com.apt.modernization.datamodel;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,12 +18,14 @@ import com.apt.modernization.datamodel.repository.QuotesRepository;
 public class PersistenceTest {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(PersistenceTest.class);
+
+	private static final int AMOUNT_MULTIPLE_INSERT=10000;
 	
 	private static final int ID_BASIC_PERSISTENCE=1;
 	
 	@Autowired
 	private QuotesRepository quotesRepository;
-	
+
 	@Test
 	public void testBasicQuotePersistence() {
 		LOG.debug("Starting basic persistence-Tests...");
@@ -44,5 +48,99 @@ public class PersistenceTest {
 		assertNotNull(readQuotes,"Could not persist new object.");
 		
 		LOG.debug("Finished basic persistence-Tests.");
+	}
+	
+	
+	@Test
+	public void testMultipleInserts() {
+		removeAllQuoteDocuments();
+		
+		StringBuilder stringBuilder=new StringBuilder();
+
+		LOG.debug("Start persisting "+AMOUNT_MULTIPLE_INSERT+" Quotes....");
+		Interval interval=new Interval();
+		for (int i=0;i<AMOUNT_MULTIPLE_INSERT;i++) {
+			stringBuilder.setLength(0);
+			stringBuilder.append("name-");
+			stringBuilder.append(i);
+			String insured=stringBuilder.toString();
+			
+			stringBuilder.setLength(0);
+			stringBuilder.append("incept-");
+			stringBuilder.append(i);
+			String incept=stringBuilder.toString();
+			
+			stringBuilder.setLength(0);
+			stringBuilder.append("expiry-");
+			stringBuilder.append(i);
+			String expiry=stringBuilder.toString();
+			
+			stringBuilder.setLength(0);
+			stringBuilder.append("currency-");
+			stringBuilder.append(i);
+			String currency=stringBuilder.toString();
+			
+			Quotes quote=new Quotes(i, insured, incept, expiry, currency);
+			quotesRepository.save(quote);
+		}
+		interval.stop();
+		LOG.debug("Finished persisting "+AMOUNT_MULTIPLE_INSERT+" Quotes. Took "+interval+"ms.");
+	}
+	
+	//
+	// Utils
+	//
+	private void removeAllQuoteDocuments() {
+		LOG.debug("Removing Quotes-Documents...");
+		Interval interval=new Interval();
+		List<Quotes> quotesList=quotesRepository.findAll();
+		for (Quotes quotes:quotesList) {
+			quotesRepository.delete(quotes);
+		}
+		interval.stop();
+		LOG.debug("Quotes-Documents are empty now. Took "+interval.getInterval()+"ms.");
+	}
+	
+	//
+	// Netsed classes
+	//
+	private static class Interval {
+		private static final Logger LOG = LoggerFactory.getLogger(Interval.class);
+		private static final long INTERVAL_INVALID=-1;
+		
+		private long start=INTERVAL_INVALID;
+		private long end=INTERVAL_INVALID;
+		
+		public Interval() {
+			start();
+		}
+		
+		public void start() {
+			start=System.currentTimeMillis();
+			end=INTERVAL_INVALID;
+		}
+		
+		public void stop() {
+			if (start==INTERVAL_INVALID) {
+				LOG.warn("Timng stopped without start. Forcing start resulting in a 0-interval.");
+				start();
+			}
+			end=System.currentTimeMillis();
+		}
+		
+		public long getInterval() {
+			long result=INTERVAL_INVALID;
+			if ((start!=INTERVAL_INVALID) && (end!=INTERVAL_INVALID))
+			{
+				result=end-start;
+			}
+			return result;
+		}
+		
+		@Override
+		public String toString() {
+			return String.valueOf(getInterval());
+		}
+		
 	}
 }
